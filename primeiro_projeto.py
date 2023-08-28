bb681e78e74045e4fc8db0c591fca7f31a2fba03
Byatriz Moura <mourabyatriz@gmail.com>
2023-08-28 15:09:58 -0300

    Update primeiro_projeto.py

--- primeiro_projeto.py
+++ primeiro_projeto.py
@@ -1,25 +1,28 @@
-#Cadastrar pessoas
+#Pegando as informações informadas pelo usuário e transformando em uma lista
 def cadastrar(pessoas):
     cpf = input("Digite seu CPF: ")
     nome = input("Digite seu NOME: ")
     idade = int(input("Digite seu IDADE: "))
     pessoas.append((cpf,nome,idade))
-
+    
+#Definindo modo de apresentação dos dados quando solicitado a listagem
 def listar(pessoas):
     for pessoa in pessoas:
         cpf, nome, idade = pessoa
         print(f"Nome {nome}, possui {idade} anos de idade e é portador(a) do CPF {cpf}")
-
+        
+#opções apresentadas no menu
 def exibir_menu():
     print(''' \n Escolha uma opção:
     1. Cadastrar pessoa
     2. Listar pessoa
     3. Sair''')
 
+#Funções executadas no menu, incluindo mensagem de erro e finalização
 def main():
     pessoas = []
     flag = True
-    while flag:  #se a tag for verdadeira
+    while flag:  
         exibir_menu()
         try:
             opcao = int(input("Digite a opção: "))
